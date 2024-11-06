f6d2b46ea8778023d08a43577cbce81246d92a6d
Nahuel Andrada <nahuel@euroasiapay.com>
2024-11-05 22:24:03 -0300

    Implements api call, chart component and loading curtain

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,7 +1,38 @@
+// Types
+import { FC } from "react";
+
+// Hooks
+import useCustomQuery from "../../hooks/useCustomQuery";
+
+// Components
+import Chart from "../../components/Chart/Chart";
+import LoadingCurtain from "../../components/LoadingCurtain/Loadingcurtain";
+
+// Utils
+import { parseNation } from "../../utils/parser";
+
+// Styles
 import styles from "./dashboard.module.css";
 
-const Dashboard = () => {
-  return <main className={styles.container}>Dashboard</main>;
+const Dashboard: FC = () => {
+  const { isPending, data } = useCustomQuery(
+    "/data?drilldowns=Nation&measures=Total%20Population&Nativity=1,2"
+  );
+
+  const parsedData = parseNation(data?.data);
+
+  if (isPending) return <LoadingCurtain />;
+
+  return (
+    <main className={styles.container}>
+      <Chart
+        data={parsedData}
+        onClick={(id: string | number) => {
+          console.log(id);
+        }}
+      />
+    </main>
+  );
 };
 
 export default Dashboard;
