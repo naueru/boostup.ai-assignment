899398774a8f556a36d3f1ce5f1177e4ae71f056
Nahuel Andrada <nahuel@euroasiapay.com>
2024-11-06 16:17:48 -0300

    Renames native for total

--- src/components/Chart/components/Legend/Legend.tsx
+++ src/components/Chart/components/Legend/Legend.tsx
@@ -4,7 +4,7 @@ import styles from "./legend.module.css";
 const Legend = () => (
   <ul className={styles.legendList}>
     <li className={styles.legendListItem}>
-      <div className={styles.identifier} /> <label>Native</label>
+      <div className={styles.identifier} /> <label>Total</label>
     </li>
     <li className={styles.legendListItem}>
       <div className={styles.identifier} /> <label>Foreign</label>
