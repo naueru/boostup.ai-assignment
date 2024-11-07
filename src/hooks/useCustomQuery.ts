cd85050ab40e2bfcab9b027f5362ff5fbd7ab3af
Nahuel Andrada <nahuel@euroasiapay.com>
2024-11-07 06:09:19 -0300

    Adds skip flag to avoid unnecessary calls

--- src/hooks/useCustomQuery.ts
+++ src/hooks/useCustomQuery.ts
@@ -4,12 +4,13 @@ import { useQuery } from "@tanstack/react-query";
 // Constants
 import { API } from "../constants/constants";
 
-const useCustomQuery = (path: string) => {
+const useCustomQuery = (path: string, skip: boolean = false) => {
   const url = API + path;
 
   return useQuery({
     queryKey: [path],
     queryFn: () => fetch(url).then((res) => res.json()),
+    enabled: !skip,
   });
 };
 
