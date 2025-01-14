a89a5067a309baa758694c221e6a720400d2d9b1
Nahuel Andrada <nahuel@euroasiapay.com>
2025-01-14 16:16:38 -0300

    Adds shared layout for entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Core
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 // Providers
 import QueryProvider from "./providers/QueryProvider";
@@ -8,19 +8,36 @@ import QueryProvider from "./providers/QueryProvider";
 import Dashboard from "./screens/Dashboard/Dashboard";
 import Detail from "./screens/Detail/Detail";
 import Error from "./screens/Error/Error";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 // Styles
 import "./App.css";
 
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
-    path: "/:stateId?",
-    element: <Dashboard />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/year/:year",
-    element: <Detail />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/:stateId?",
+        element: <Dashboard />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/year/:year",
+        element: <Detail />,
+      },
+    ],
   },
 ]);
 
