b834acb36a4bc66fec723dbfc736ecc7681869d9
Nahuel Andrada <nahuel@euroasiapay.com>
2025-01-14 16:23:46 -0300

    Implements graphic types options for chart component

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -2,12 +2,14 @@
 import { FC, useMemo } from "react";
 
 // Components
-import Bar from "./components/Bar/Bar";
+import HybridGraphic from "./components/HybridGraphic/HybridGraphic";
+import BarsGraphic from "./components/BarsGraphic/BarsGraphic";
 import Labels from "./components/Labels/Labels";
 import Legend from "./components/Legend/Legend";
 import Title from "../Title/Title";
 
 // Utils
+import { getNextRoundNumber } from "../../utils/math";
 import { IParsedElement } from "../../utils/parser";
 
 // Styles
@@ -17,20 +19,23 @@ export type TChart = {
   data: IParsedElement[];
   onClick: (id: number | string) => void;
   label: string;
+  graph: number;
 };
 
-const Chart: FC<TChart> = ({ onClick, data, label }) => {
+const Chart: FC<TChart> = ({ onClick, data, label, graph }) => {
   const reversed = [...data].reverse();
   const ids = reversed.map((el) => el.label);
   const legendLabels = (data[0]?.data && Object.keys(data[0]?.data)) || [];
 
   const max = useMemo(
     () =>
-      Math.max(
-        ...reversed.reduce<number[]>((acc, current) => {
-          const values = Object.values(current.data);
-          return [...acc, ...values];
-        }, [])
+      getNextRoundNumber(
+        Math.max(
+          ...reversed.reduce<number[]>((acc, current) => {
+            const values = Object.values(current.data);
+            return [...acc, ...values];
+          }, [])
+        )
       ),
     [reversed]
   );
@@ -43,20 +48,12 @@ const Chart: FC<TChart> = ({ onClick, data, label }) => {
           <label>{max}</label>
           <label>0</label>
         </div>
-        <div className={styles.chartContainer}>
-          <div className={styles.chart}>
-            {reversed &&
-              reversed.map((el, index) => (
-                <Bar
-                  onClick={onClick}
-                  key={`bar-${el.label}-${index}`}
-                  id={el.label}
-                  max={max}
-                  data={Object.entries(el.data)}
-                />
-              ))}
-          </div>
-        </div>
+
+        {graph ? (
+          <BarsGraphic data={data} onClick={onClick} max={max} />
+        ) : (
+          <HybridGraphic data={data} onClick={onClick} max={max} />
+        )}
         <div className={styles.labels}>
           <Labels data={ids} />
         </div>
