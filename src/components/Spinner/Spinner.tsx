b179c4a19a46867de85678f1c686d57ae44b7490
Nahuel Andrada <nahuel@euroasiapay.com>
2025-01-14 16:39:50 -0300

    Adds logic to allow multiple sizes

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,9 +4,29 @@ import { FC } from "react";
 // Styles
 import styles from "./spinner.module.css";
 
-const Spinner: FC = () => {
+export type TSpinner = {
+  size?: "s" | "S" | "m" | "M" | "l" | "L";
+};
+
+const Spinner: FC<TSpinner> = ({ size }) => {
+  let scale: number;
+
+  switch (size) {
+    case "s":
+    case "S":
+      scale = 0.4;
+      break;
+    case "l":
+    case "L":
+      scale = 1.5;
+      break;
+    default:
+      scale = 1;
+      break;
+  }
+
   return (
-    <div className={styles.spinner}>
+    <div className={styles.spinner} style={{ transform: `scale(${scale})` }}>
       <div></div>
       <div></div>
       <div></div>
